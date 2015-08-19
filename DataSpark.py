62a70553b4e7005caff4b355a08062c29fd3d26b
shaunpanjabi <shaun14@gmail.com>
2015-08-18 21:37:57 -0700

    Added comments, kwargs for send_data, and minor cleanup

--- DataSpark.py
+++ DataSpark.py
@@ -24,10 +24,19 @@ BASE_URL = "https://data.sparkfun.com/"
 
 class DataSparker():
     """
-    Send and receive data
+    Send and receive data to a Phant server. This doesn't only have to be used for SparkFun. You can deploy your own
+    Phant server if you prefer to keep your data to yourself. See link above for info on customizing your Phant Server.
+
     """
     def __init__(self, public_key, private_key, delete_key=None, base_url=BASE_URL,):
+        """
+        Constructor for DataSparker class
 
+        :param public_key: {string} Public key for SparkFun data stream
+        :param private_key: {string} Private key for SparkFun data stream.
+        :param delete_key: {string} Delete key for SparkFun data stream.
+        :param base_url: {string} SparkFun base URL (Only change if you're deploying your own Phant server).
+        """
         self.base_url = base_url
         self.public_key = public_key
         self.private_key = private_key
@@ -35,10 +44,18 @@ class DataSparker():
 
         self.available_formats = ['json', 'csv', 'sql', 'psql', 'atom']
 
-    def send_data(self, params):
+    def send_data(self, **kwargs):
+        """
+        Send data to data stream.
+
+        :param: Whatever data you want to send. For example, if your data stream has the fields duration and distance,
+                you would do:
+                    send_data(duration=value1, distance=value2)
+        :return: Response from request. If not successful, False is returned.
+        """
         request_type = "input/"
         url = self.base_url + request_type + self.public_key + '?' + 'private_key=' + self.private_key
-        response = requests.post(url, params)
+        response = requests.post(url, kwargs)
         if response.status_code != 200:
             print ("Request was not successful:\n" + response.text)
             return False
@@ -47,7 +64,15 @@ class DataSparker():
             return response
 
     def download_data(self, data_format='json'):
+        """
+        Download all data from a data stream.
+
+        :param data_format: {string} See available_formats, for allowed data formats.
+        :return: If json is selected, a {dictionary} is returned. Otherwise the data is returned in a {string}.
+                 This can be manually saved to a file. Eventually, save functionality will be added.
+        """
         # TODO: Add save to file option
+        # TODO: Add page selection option
         response_type = "output/"
         if data_format not in self.available_formats:
             raise Exception("Pick from available formats: " + str(self.available_formats))
@@ -59,6 +84,13 @@ class DataSparker():
         return resp
 
     def clear_stream(self):
+        """
+        CAUTION: USE AT YOUR OWN RISK!!
+        This actually clears your entire stream, and cannot be undone. All data is wiped, however you can still upload
+        new data to the stream if you would like.
+
+        :return: {bool} If clear succeeds, True is returned
+        """
         clear_status = False
         request_type = ['input/', '/clear']
         url = self.base_url + request_type[0] + self.public_key + request_type[1] + '?private_key=' + self.private_key
@@ -105,22 +137,3 @@ class DataSparker():
         url = self.base_url + request_type[0] + self.public_key + request_type[1]
         response = requests.get(url)
         return json.loads(response.text)
-
-# ds = DataSparker(delete_key=Delete_key, public_key=Public_key, private_key=Private_key)
-#
-# # print ds.clear_stream()
-# # raw_input("Check if stream is clear...")
-# # for i in xrange(100):
-# #     data_packet = {
-# #         'test1': i,
-# #         'test2': i+1,
-# #         'test3': i+2,
-# #     }
-# #     print ds.send_data(data_packet)
-#
-# print ds.download_data()
-# # print ds.download_data(data_format="csv")
-#
-# print ds.get_stream_stats()
-# print ds.delete_stream(confirm=True)
-# print ds.get_stream_stats()
